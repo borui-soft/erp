30aa418bccc8073c8730ace795a87c523020b139
borui-soft <2870995167@qq.com>
2017-06-25 11:32:36 +0800

    版本号升级至V2.5.58 此版本较上一版本增加如下功能 1、所有销售类单据数据录入页面，增加品牌栏目，包含销售订单、销售出库单、领料单、盘亏毁损单、其他出库单 2、材料变更单套打模板中增加了部分名称和所属部件信息 3、其他出库单套打模板增加项目编号和生产编号信息 4、针对盘亏亏损单，当单据为已审核时，在单据右上角显示已审核字样 5、在各类单据中，需要选择部门时，在新打开的部门信息页面中，默认展开部门信息树节点

--- code/code/borui/app/企业版/MainProgram/view/主界面/FormAbout.Designer.cs
+++ code/code/borui/app/企业版/MainProgram/view/主界面/FormAbout.Designer.cs
@@ -73,7 +73,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(131, 12);
             this.label3.TabIndex = 15;
-            this.label3.Text = "当前软件版本：V2.5.50";
+            this.label3.Text = "当前软件版本：V2.5.58";
 
             // 
             // richTextBox1
@@ -82,10 +82,11 @@
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(385, 90);
             this.richTextBox1.TabIndex = 19;
-            this.richTextBox1.Text = "1、预占申请单：解决当单据物料数目大于12条时，页面无法打开的问题\r\n";
-            this.richTextBox1.Text += "2、库存预占序时薄：不在显示预占数量为0的物料预占信息\r\n";
-            this.richTextBox1.Text += "3、xx材料表:解决单据在未审核的状态下，无法修改设计人的问题\r\n";
-            this.richTextBox1.Text += "4、解决领料到审核后，自动消除库存占用数量再某些情况下错误的问题，变量类型有double修改为decimal \r\n";
+            this.richTextBox1.Text = "1、所有销售类单据数据录入页面，增加品牌栏目，包含销售订单、销售出库单、领料单、盘亏毁损单、其他出库单\r\n";
+            this.richTextBox1.Text += "2、材料变更单套打模板中增加了部分名称和所属部件信息\r\n";
+            this.richTextBox1.Text += "3、其他出库单套打模板增加项目编号和生产编号信息\r\n";
+            this.richTextBox1.Text += "4、针对盘亏亏损单，当单据为已审核时，在单据右上角显示已审核字样\r\n";
+            this.richTextBox1.Text += "5、在各类单据中，需要选择部门时，在新打开的部门信息页面中，默认展开部门信息树节点\r\n";
 
             // Fo
             // 
